8d49c74684e264e450dc7603cf02ee166e11b7ae
Pallab Maiti <mynamepallabmaiti@gmail.com>
2023-03-31 15:13:23 +0530

    feat: upgraded Bugsnag(5.29.0) and Rudder(1.12.0) SDK

--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.rudderstack.android.sample.kotlin
 
 import android.app.Application
+import com.rudderlabs.android.sample.kotlin.BuildConfig
 import com.rudderstack.android.integrations.bugsnag.BugSnagIntegrationFactory
 import com.rudderstack.android.sdk.core.RudderClient
 import com.rudderstack.android.sdk.core.RudderConfig
@@ -8,9 +9,6 @@ import com.rudderstack.android.sdk.core.RudderLogger
 
 class MainApplication : Application() {
     companion object {
-        private const val WRITE_KEY = "1ZIkdsNVvxrUC7bRGOorIxmJVEH"
-        private const val DATA_PLANE_URL = "https://485fdffb.ngrok.io"
-        private const val CONTROL_PLANE_URL = "https://485fdffb.ngrok.io"
         lateinit var rudderClient: RudderClient
     }
 
@@ -18,10 +16,9 @@ class MainApplication : Application() {
         super.onCreate()
         rudderClient = RudderClient.getInstance(
             this,
-            WRITE_KEY,
+            BuildConfig.WRITE_KEY,
             RudderConfig.Builder()
-                .withDataPlaneUrl(DATA_PLANE_URL)
-                .withControlPlaneUrl(CONTROL_PLANE_URL)
+                .withDataPlaneUrl(BuildConfig.DATA_PLANE_URL)
                 .withFactory(BugSnagIntegrationFactory.FACTORY)
                 .withLogLevel(RudderLogger.RudderLogLevel.VERBOSE)
                 .build()
